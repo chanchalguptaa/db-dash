d3444c8a481f5dba19ec99ab9505589b0953d126
tanish510 <tanishjain510@gmail.com>
2023-02-14 12:56:15 +0530

    create table api done

--- controllers/tableController.js
+++ controllers/tableController.js
@@ -1,8 +1,6 @@
 const { prepareErrorResponse, prepareSuccessResponse } = require("../services/utilityService.js");
-const sqlDbService = require("../sql_db_services/databaseService")
 const {addTable,getById} = require("../db_services/masterDbService");
 const tableService = require("../sql_db_services/tableService.js") 
-const { Client } = require('pg');
 const createTable = async (req,res)=>{
     try{
          const db_id = req?.body?.id;
@@ -10,8 +8,7 @@ const createTable = async (req,res)=>{
          const data = await getById(db_id);
          const ans = await tableService.createTableService(tableName,data)
          try {
-            
-            const data = await addTable(db_id,{"1234":{tableName:tableName}})
+            const data1 = await addTable(db_id,tableName)
              return res.status(200).json(prepareSuccessResponse({ message: `Table '${tableName}' created successfully`}))
          }
          catch (err) {
