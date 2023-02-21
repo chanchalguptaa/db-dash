5c372dea5c7fb1691987c948bdfa17467d03ec52
Ranu verma <verma2001ranu@gmail.com>
2023-02-21 09:50:48 +0530

    changing in view

--- controllers/fieldController.js
+++ controllers/fieldController.js
@@ -12,28 +12,28 @@ const createField = async (req, res) => {
     const fieldType = req?.body?.fieldType;
     try {
          const data = await getById(db_id);
-         console.log("data in create table ", data);
-
+         
          const ans = await fieldService.createFieldService(tableName, fieldName,fieldType,data)
          try { 
-             //add to mongo 
                  const tmp=data?.tables?.[tableName].fields?.[fieldName]||null;
                  if(!tmp)  
-                 {  const data1=await addField(db_id,tableName,fieldName,fieldType);
-
+               {  
+                    const data1=await addField(db_id,tableName,fieldName,fieldType);
                }
                else
-               { return res.status(404).json(prepareErrorResponse({ message: `Field ${fieldName}  not exits in  table ${tableName}` }))
+              { 
+                   return res.status(404).json(prepareErrorResponse({ message: `Field ${fieldName}  not exits in  table ${tableName}` }))
               }
+
               return res.status(200).json(prepareSuccessResponse({ message: `Field '${fieldName}' created successfully` }))
          }
          catch (err) {
-              return res.status(400).json(prepareErrorResponse({ message: `Error creating field ${err.message}` }));
-         }
+                         return res.status(400).json(prepareErrorResponse({ message: `Error creating field ${err.message}` }));
+                     }
     }
     catch (err) {
-         return res.status(400).json(prepareErrorResponse({ message: `Error creating field ${err.message}` }));
-    }
+                   return res.status(400).json(prepareErrorResponse({ message: `Error creating field ${err.message}` }));
+                 }
 
 }
 
@@ -41,7 +41,7 @@ const deleteField = async (req, res) => {
     const db_id = req?.params?.dbId;
     const tableName = req?.params?.tableName;
     const fieldName = req?.body?.fieldName;
-    const fieldType=req?.body?.fieldType;
+    const fieldType =req?.body?.fieldType;
     try { 
          const data = await getById(db_id);
          const ans = await fieldService.deleteFieldService(tableName, fieldName,data)
