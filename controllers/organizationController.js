1519d3a5187f71f1682262128d38177b1db55c56
Chanchal Gupta <mvsr1303@gmail.com>
2023-03-01 11:28:57 +0530

    minor changes

--- controllers/organizationController.js
+++ controllers/organizationController.js
@@ -55,7 +55,7 @@ const createOrg = async (req, res) => {
          if(ifUser != null)
          {
            const orgData =  await orgService.saveOrg(org,user_id);
-           await addDefaultdbInOrg (orgData._id,"untitled Db",user_id);
+           await addDefaultdbInOrg (orgData._id,"untitledDb",user_id);
             return res.status(200).json(prepareSuccessResponse({ data: orgData, message: "successfully created organization" }));
          }
          else {
