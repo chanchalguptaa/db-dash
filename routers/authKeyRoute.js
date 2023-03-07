d88046fd41d3ae255725f28c22fc31808fa1b1d3
hariomm22 <hariommahajan22.com>
2023-03-07 12:38:03 +0530

    auth keydelete api done

--- routers/authKeyRoute.js
+++ routers/authKeyRoute.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const { createAuthKey,deleteAuthKey,updateAuthKey } = require("../controllers/authKeyController")
 
 router.route('/:dbId/admin/:adminId/authkey').post(createAuthKey)
-router.route('/:dbId/:authkey/:adminId/updateauthkey').patch(updateAuthKey)
-router.route('/:dbId/:authkey/:adminId/deleteauthkey').delete(deleteAuthKey)
+router.route('/:dbId/admin/:adminId/:authkey/updateAuthkey').patch(updateAuthKey)
+router.route('/:dbId/admin/:adminId/:authkey/deleteAuthkey').delete(deleteAuthKey)
+// http://localhost:5000/dbs/63ee161e728e3e9ab145f0e7/admin/63e603b25514ae381dfda3fb/AUR0fX7X/updateAuthkey
 module.exports = router;
